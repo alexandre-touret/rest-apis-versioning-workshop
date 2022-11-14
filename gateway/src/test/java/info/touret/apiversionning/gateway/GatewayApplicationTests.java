ba9beac424f89421f29ddba6684c6a2329f4cf01
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2022-11-14 21:43:50 +0100

    fix unit test

--- gateway/src/test/java/info/touret/apiversionning/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/info/touret/apiversionning/gateway/GatewayApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 
 @SpringBootTest(classes = NoSecurityConfiguration.class)
@@ -13,6 +14,9 @@ class GatewayApplicationTests {
 
 	@MockBean
 	private ReactiveJwtDecoder reactiveJwtDecoder;
+
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@BeforeEach
 	void setUp() {
