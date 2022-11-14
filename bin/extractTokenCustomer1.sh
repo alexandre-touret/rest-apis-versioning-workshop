4f5ff2b9d639f744da11cdebb3a5d0a91596f0fe
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2022-11-14 21:13:55 +0100

    feat(security) first configuration

--- bin/extractTokenCustomer1.sh
+++ bin/extractTokenCustomer1.sh
@@ -1,1 +1,1 @@
-http --form post :8009/oauth2/token grant_type="client_credentials" client_id="customer1" client_secret="secret" --print b | jq '.access_token'  > token.txt
+http --form post :8009/oauth2/token grant_type="client_credentials" client_id="customer1" client_secret="secret" --print b | jq -r '.access_token'  > token.txt
