2ae85e9cea17d0a14f065be6d0d482b955e3e8e3
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2022-11-14 21:36:24 +0100

    feat(security) first configuration

--- gateway/src/test/java/info/touret/apiversionning/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/info/touret/apiversionning/gateway/GatewayApplicationTests.java
@@ -1,10 +1,22 @@
 package info.touret.apiversionning.gateway;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 
-@SpringBootTest
+@SpringBootTest(classes = NoSecurityConfiguration.class)
+@Profile("test")
 class GatewayApplicationTests {
+
+	@MockBean
+	private ReactiveJwtDecoder reactiveJwtDecoder;
+
+	@BeforeEach
+	void setUp() {
+	}
 
 	@Test
 	void contextLoads() {
