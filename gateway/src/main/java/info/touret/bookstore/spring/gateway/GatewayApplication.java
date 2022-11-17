b2323788b22fe22bce3be5c565f3a7b9be69867e
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2022-11-17 15:26:33 +0100

    fix: default security configuration

--- gateway/src/main/java/info/touret/bookstore/spring/gateway/GatewayApplication.java
+++ gateway/src/main/java/info/touret/bookstore/spring/gateway/GatewayApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -28,7 +27,7 @@ public class GatewayApplication {
      */
     @Bean
     SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) {
-        /* Defaut configuration for OAUTH authorization (TO BE ADDED during the workshop)*/
+        /* Defaut configuration for OAUTH authorization (TO BE ADDED during the workshop)
         http.csrf().disable().cors().disable()
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/book/**").hasAnyAuthority("SCOPE_book:read", "SCOPE_book:write")
@@ -38,9 +37,9 @@ public class GatewayApplication {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults())
                 );
-
-        /* If the previous configuration is applied, you would remove this following line (and the other way around)
-        http.csrf().disable().cors().disable().authorizeExchange().anyExchange().permitAll();*/
+        */
+        /* If the previous configuration is applied, you would remove this following line (and the other way around) */
+        http.csrf().disable().cors().disable().authorizeExchange().anyExchange().permitAll();
         return http.build();
     }
 
