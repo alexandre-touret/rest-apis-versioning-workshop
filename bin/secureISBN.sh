4c740fb5e9da98a5030a1977e8be7ec0ab2e0b88
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2022-12-16 13:51:53 +0100

    fix: typos in shell script

--- bin/secureISBN.sh
+++ bin/secureISBN.sh
@@ -2,5 +2,4 @@
 
 
 access_token=`http --form post :8009/oauth2/token grant_type="client_credentials" client_id="customer2" client_secret="secret2" scope="openid number:read" -p b | jq -r '.access_token'`
-echo $access_token
 http :8080/isbns "Authorization: Bearer ${access_token}"
