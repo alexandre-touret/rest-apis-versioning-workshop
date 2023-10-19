11bd3daeec34fef166c7347b9fe1eb150027d0b4
Alexandre Touret <alexandre-touret@users.noreply.github.com>
2023-10-19 18:22:30 +0200

    fix:typos

--- bin/secureISBN-header.sh
+++ bin/secureISBN-header.sh
@@ -2,5 +2,5 @@
 
 
 access_token=`http --form post :8009/oauth2/token grant_type="client_credentials" client_id="customer2" client_secret="secret2" scope="openid" -p b | jq -r '.access_token'`
-echo $access_token
+
 http :8080/isbns "X-API-VERSION: v1" "Authorization: Bearer ${access_token}"
